1e72c7317cdd7a25a425787441f57f3dc048b632
EugenRakhimov <gin3002@gmail.com>
2016-04-23 13:40:04 +1200

    something working

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,11 +2,13 @@ require 'net/http'
 class VisitorsController < ApplicationController
   before_action :authenticate_user!
   def index
-    uri = URI('http://asterank.com/api/asterank?query={"e":{"$lt":0.1},"i":{"$lt":4},"a":{"$lt":1.5}}&limit=1')
-    Net::HTTP.start(uri.host, uri.port) do |http|
-      request = Net::HTTP::Get.new uri
-      response = http.request request # Net::HTTPResponse object
-      p response
-    end
+    uri = URI('http://www.asterank.com/api/asterank?query={%22e%22:{%22$lt%22:0.1},%22i%22:{%22$lt%22:4},%22a%22:{%22$lt%22:1.5}}&limit=100')
+    # Net::HTTP.start(uri.host, uri.port) do |http|
+    #   request = Net::HTTP::Get.new uri
+    #   response = http.request request # Net::HTTPResponse object
+    #   p response
+    # end
+    res = Net::HTTP.get_response(uri)
+    p res.body
   end
 end

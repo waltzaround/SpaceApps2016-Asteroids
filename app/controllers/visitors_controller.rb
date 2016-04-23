1d24bd3107e8d8046a4915ec397d76d64c045b95
EugenRakhimov <gin3002@gmail.com>
2016-04-23 12:49:19 +1200

    started to investigate API

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,12 @@
+require 'net/http'
 class VisitorsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    uri = URI('http://asterank.com/api/asterank?query={"e":{"$lt":0.1},"i":{"$lt":4},"a":{"$lt":1.5}}&limit=1')
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      request = Net::HTTP::Get.new uri
+      response = http.request request # Net::HTTPResponse object
+      p response
+    end
+  end
 end

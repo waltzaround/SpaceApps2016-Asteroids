8a57340e17a523c4d6e5306e7743c1148b7ad02a
EugenRakhimov <gin3002@gmail.com>
2016-04-23 17:10:16 +1200

    profitable asteroid search

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,7 +3,7 @@ require 'json'
 class VisitorsController < ApplicationController
   before_action :authenticate_user!
   def index
-    uri = URI('http://www.asterank.com/api/asterank?query={%22e%22:{%22$lt%22:0.1},%22i%22:{%22$lt%22:4},%22a%22:{%22$lt%22:1.5}}&limit=100')
+    uri = URI('http://www.asterank.com/api/asterank?query={%22e%22:{%22$lt%22:0.1},%22i%22:{%22$lt%22:10},%22a%22:{%22$lt%22:7.5},%22profit%22:{%22$gt%22:0}}&limit=100')
     # Net::HTTP.start(uri.host, uri.port) do |http|
     #   request = Net::HTTP::Get.new uri
     #   response = http.request request # Net::HTTPResponse object
@@ -11,6 +11,6 @@ class VisitorsController < ApplicationController
     # end
     res = Net::HTTP.get_response(uri)
     @asteroids = JSON.parse(res.body)
-    p @asteroids
+    # p @asteroids
   end
 end
